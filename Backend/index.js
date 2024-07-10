51acba9dae3de886f8a43aa4786d3fa25390eea3
prashuk jain <29jainprashuk@gmail.com>
2024-07-10 16:30:15 +0530

    Updated Backend / route

--- Backend/index.js
+++ Backend/index.js
@@ -8,6 +8,14 @@ const port = 3000;
 app.use(cors())
 app.use("/api", GetDataRoutes);
 
+app.get('/' , (req , res)=>{
+  res.json({
+    "message": 'Welcome To AlloHealth Backend',
+    "To Get Meals" : '/api/getmeals',
+    "To Get tags" : '/api/gettags'
+  })
+})
+
 app.listen(port, () => {
   console.log(`server is running at port ${port}`);
 });
